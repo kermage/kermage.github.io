6738158cac6a48f5550f3a0e20209c01f72a8c5d
kermage <genealyson.torcende@gmail.com>
2017-04-16 06:42:58 +0800

    Initial build; base files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*!
+ *  kermage.github.io 0.1.0
+ *  Copyright (c) 2017 Gene Alyson Fortunado Torcende
+ *  Licensed under MIT (https://github.com/kermage/kermage.github.io/blob/master/LICENSE)
+ */
+
+(function() {
+	'use strict';
+
+}());
+
+//# sourceMappingURL=main.js.map
