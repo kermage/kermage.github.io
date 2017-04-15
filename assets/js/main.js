71dfa650c7369f794820b1177d5e61a6811660f4
kermage <genealyson.torcende@gmail.com>
2017-04-16 06:39:36 +0800

    Initial commit; base files

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*!
+ *  kermage.github.io 0.1.0
+ *  Copyright (c) 2017 Gene Alyson Fortunado Torcende
+ *  Licensed under MIT (https://github.com/kermage/kermage.github.io/blob/master/LICENSE)
+ */
+
+(function() {
+	'use strict';
+
+}());
